874411a7bf299bbd952f0e4e32000ec82b67e98b
shubham agrawal <shubham.agrawal@students.iiit.ac.in>
2021-05-22 20:20:38 +0530

    Added assignments

---
+++ Assignment1/src/coord_image.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+img = plt.imread("../calibration-data/calib-object.jpg")
+
+plt.imshow(img)
+plt.show()
+#https://step-icicihr-c.allincall.in/applicant/custom-authentication/
